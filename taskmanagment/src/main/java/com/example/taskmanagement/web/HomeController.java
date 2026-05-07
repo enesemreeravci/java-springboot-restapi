fd4cb2667c1642bb31048f4e158add7656010865
Enes Emre Eravci <enes.nev@gmail.com>
2026-05-07 22:17:08 +0200

    added new features, ui designed with css.

--- taskmanagment/src/main/java/com/example/taskmanagement/web/HomeController.java
+++ taskmanagment/src/main/java/com/example/taskmanagement/web/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.taskmanagement.web;
 
 import com.example.taskmanagement.service.TaskService;
+import org.springframework.boot.Banner;
+import org.springframework.data.convert.JMoleculesConverters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -46,5 +48,25 @@ public class HomeController {
         service.toggleCompleted(id);
         return "redirect:/";
     }
+
+    @GetMapping("/tasks/edit/{id}")
+    public String showEditForm(@PathVariable Long id, Model model)
+    {
+        Task task = service.getTaskById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+
+        model.addAttribute("task", task);
+
+        return "edit-task";
+    }
+
+    @PostMapping("/tasks/update/{id}")
+    public String updateTask(@PathVariable Long id, Task task)
+    {
+        service.updateTask(id, task);
+
+        return "redirect:/";
+    }
+
+
 
 }
